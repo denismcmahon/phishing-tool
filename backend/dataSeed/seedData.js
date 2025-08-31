1828f1ab634b98ba2d8ec20da70b0b14e682e56d
Denis McMahon <denismcmahon@gmail.com>
2025-08-31 16:09:55 +0100

    chore: extend seedData script to include templates

--- backend/dataSeed/seedData.js
+++ backend/dataSeed/seedData.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const User = require('../models/User');
 const Campaign = require('../models/Campaign');
+const Template = require('../models/Template');
 
 const seedData = async () => {
   try {
@@ -13,6 +14,9 @@ const seedData = async () => {
     // clear old data
     await User.deleteMany();
     await Campaign.deleteMany();
+    await Template.deleteMany();
+
+    console.log('Cleared existing collections');
 
     // create users
     const users = await User.insertMany([
@@ -34,11 +38,52 @@ const seedData = async () => {
     });
 
     await campaign.save();
+    console.log(`Seeded campaign: ${campaign.name}`);
 
-    console.log('Seeded campaign:', campaign.name);
+    // create email templates
+    const templates = await Template.insertMany([
+      {
+        name: 'Invoice Scam',
+        subject: 'Urgent: Outstanding Invoice Payment Required',
+        bodyHtml: `
+          <p>Dear User,</p>
+          <p>Our records indicate that you have an outstanding invoice that requires immediate attention.</p>
+          <p><a href="http://phishing-link.com">Click here</a> to review and settle your balance.</p>
+          <p>Thank you,</p>
+          <p>Finance Department</p>
+        `,
+        bodyText:
+          'Dear User,\n\nOur records indicate you have an outstanding invoice. Visit http://phishing-link.com to review and settle your balance.\n\nFinance Department',
+      },
+      {
+        name: 'Password Reset',
+        subject: 'Password Reset Request',
+        bodyHtml: `
+          <p>Hello,</p>
+          <p>We received a request to reset your account password. If this was you, please <a href="http://phishing-link.com/reset">reset your password here</a>.</p>
+          <p>If not, please ignore this email.</p>
+        `,
+        bodyText:
+          'Hello,\n\nWe received a request to reset your password. Reset at http://phishing-link.com/reset\n\nIf not, ignore this email.',
+      },
+      {
+        name: 'HR Alert',
+        subject: 'Important HR Policy Update',
+        bodyHtml: `
+          <p>Dear Employee,</p>
+          <p>Our HR department has released a new policy update. Please <a href="http://phishing-link.com/hr">review the changes here</a>.</p>
+          <p>Best,</p>
+          <p>HR Department</p>
+        `,
+        bodyText:
+          'Dear Employee,\n\nHR has released a new policy update. Review here: http://phishing-link.com/hr\n\nHR Department',
+      },
+    ]);
+
+    console.log(`Seeded ${templates.length} templates`);
 
     mongoose.connection.close();
-    console.log('Seeding complete ');
+    console.log('Seeding complete');
   } catch (err) {
     console.error('Seeding error:', err);
     mongoose.connection.close();
